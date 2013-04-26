65c4d2feeed380813ff9d09ebfdabc5266d91fab
pratik <pratik@joshsoftware.com>
2013-04-26 15:57:36 +0530

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booking::Application.config.secret_token = 'c1642851e25ef8d0f5512b045ff7e7dbc618eb512094d788b312fd3b59d0938217d1731b2e22ff057518d74b8bddd7c9b5f0e9373d7a324fa6722d11ca9c899f'
